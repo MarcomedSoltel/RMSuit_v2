e757805f42f40f1535993fa6e620be7381284c21
MarcomedSoltel <marco.mediano@soltel.es>
2024-07-24 14:30:07 +0200

    Add files via upload

---
+++ RMSuit_v2/RMSuit_v2/Models/Table.cs
@@ -0,0 +1,15 @@
+﻿namespace RMSuit_v2.Models
+{
+    public enum TableState
+    {
+        Neutral,
+        Ocupado,
+        Reservado
+    }
+
+    public class Table
+    {
+        public string Number { get; set; } = string.Empty; // Valor predeterminado
+        public TableState State { get; set; } = TableState.Neutral;
+    }
+}
---
+++ RMSuit_v2/RMSuit_v2/Services/DataService.cs
@@ -0,0 +1,29 @@
+﻿using PruebaConBlazor.Constants;
+
+namespace PruebaConBlazor.Services
+{
+    public class DataService
+    {
+        public bool MostrarError { get; set; } = true;
+
+        public List<string> TablasDiario => DatabaseTables.TablasDiario;
+
+        public List<string> TablasHistorico => DatabaseTables.TablasHistorico;
+
+        public const int CM_RESTORE = MessageIds.CM_RESTORE;
+
+        public char[] SerieAlfaNumerica => Series.SerieAlfaNumerica;
+
+        public char[] SerieFidelizacion => Series.SerieFidelizacion;
+
+        public char[] SerieSecurity => Series.SerieSecurity;
+
+        public char[] SerieBasico => Series.SerieBasico;
+
+        public char[] SerieFastMaster => Series.SerieFastMaster;
+
+        public int MaxTipoFormato => Constants.TipoFormato.MaxTipoFormato;
+
+        public string[,] TipoFormato => Constants.TipoFormato.Formatos;
+    }
+}
